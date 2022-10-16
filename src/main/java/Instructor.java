e1df8daeba7367f283b7f4fd4134e3d3435ccaeb
JazminRondon <Jazmin_Rondon@outlook.com>
2022-10-16 16:37:10 -0400

    part 5

--- src/main/java/Instructor.java
+++ src/main/java/Instructor.java
@@ -3,11 +3,13 @@ public class Instructor extends Person implements Teacher{
 
     @Override
     public void teach(Learner learner, Double numberOfHours) {
-        
+            learner.learn(numberOfHours);
     }
 
     @Override
     public void lecture(Learner[] learners, double numberOfHours) {
-
+        for (int i = 0; i < learners.length; i++) {
+            learners[i].learn(numberOfHours/ learners.length);
+        }
     }
 }
---
+++ src/test/java/TestInstructor.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+public class TestInstructor {
+
+
+
+    @Test
+    public void testImplementation(){
+
+
+
+
+
+    }
+
+
+
+
+}
